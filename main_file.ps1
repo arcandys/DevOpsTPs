7c449900690f6b6ec5f966868bb1ad3af0fc1153
lu <66738996+arcandys@users.noreply.github.com>
2024-01-19 14:38:56 +0100

    Update main_file.ps1

--- main_file.ps1
+++ main_file.ps1
@@ -1,9 +1,5 @@
 Write-Host “Task1”
 Write-Host “Task2”
 Write-Host “Task3”
-<<<<<<< HEAD
 Write-Host “Task4”
 Write-Host “Task5”
-=======
-Write-Host “Task5”
->>>>>>> 076012c9f75e48f8f4bce8215e881238c0fa8056
